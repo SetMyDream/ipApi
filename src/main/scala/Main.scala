1ba853b77a2d434755cb9434c7232a02e83b8fb4
Artem <artem.vorobiov@sigma.software>
2023-07-24 21:44:12 +0300

    update getIPAddressFromApi to return Option[String] instead of Task.effect in purpose for easier further testing

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,15 @@
 import com.fasterxml.jackson.databind.ObjectMapper
 
 import scala.io.Source
-import zio.*
-import zio.console.*
+import zio._
+import zio.console._
 
 import java.io.IOException
 
 
 object Main extends App {
 
-  def getIPAddressFromApi: Task[Option[String]] = Task.effect {
+  def getIPAddressFromApi: Option[String] = {
     val apiUrl = "https://api.ipify.org/?format=json"
     try {
       val jsonString = Source.fromURL(apiUrl).mkString
@@ -24,15 +24,23 @@ object Main extends App {
     val mapper = new ObjectMapper()
     try {
       val jsonNode = mapper.readTree(str)
-      jsonNode.get(key).asText()
+      val node = jsonNode.get(key)
+      if (node == null) {
+        throw new NoSuchElementException(s"Key '$key' not found in JSON.")
+      } else {
+        node.asText()
+      }
     } catch {
-      case _: IOException => throw new RuntimeException("Error parsing JSON")
+      case _: IOException => throw new NoSuchElementException("Error parsing JSON")
     }
   }
 
   val program: ZIO[Console, Throwable, Unit] = for {
-    ipAddress <- getIPAddressFromApi
-    _ <- putStrLn(ipAddress.getOrElse("Error fetching IP address from the API."))
+    ipAddress <- ZIO.effectTotal(getIPAddressFromApi)
+    _ <- ipAddress match {
+      case Some(ip) => putStrLn(ip)
+      case None => putStrLn("Error fetching IP address from the API.")
+    }
   } yield ()
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
