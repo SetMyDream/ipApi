4f9307ec8c151e7feab47e56410672d07ff806f2
Artem <artem.vorobiov@sigma.software>
2023-07-24 22:08:50 +0300

    Tests w/o mocking: simple tests for jsonParser function

--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -1,9 +1,10 @@
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.matchers.should.Matchers.shouldBe
-import zio._
-import zio.test._
-import zio.test.Assertion._
+import zio.*
+import zio.test.*
+import zio.test.Assertion.*
+import zio.test.environment.{TestConsole, TestEnvironment}
 
 class MainSpec extends AnyFlatSpec with Matchers {
 
@@ -16,15 +17,21 @@ class MainSpec extends AnyFlatSpec with Matchers {
     result shouldBe "1.1.1.1"
   }
 
-  "getIPAddressFromApi" should "return the IP address from the API response" in {
-    // Mock the API response with a sample JSON string
-    val mockResponse =
-      """{"ip":"188.163.96.72"}"""
+  it should "throw an exception if the key is not found case bad key" in {
+    val jsonString = """{"ip":"1.1.1.1"}"""
+    val key = "wrong_key"
 
-    // Call the function with the mock response
-    val result = Main.getIPAddressFromApi
+    assertThrows[NoSuchElementException] {
+      Main.parseJson(jsonString, key)
+    }
+  }
 
-    // Assert the result is as expected using ZIO Test's assert method
-    assertM(result)(isSome(equalTo("188.163.96.72")))
+  it should "throw an exception if the key is not found case bad json" in {
+    val jsonString = """{"ips":"1.1.1.1"}"""
+    val key = "ip"
+
+    assertThrows[NoSuchElementException] {
+      Main.parseJson(jsonString, key)
+    }
   }
 }
